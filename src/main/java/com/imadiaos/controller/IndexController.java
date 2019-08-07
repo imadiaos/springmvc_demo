f7098103e84f67c965de159e37e8aab5a7480ab6
wangyn <wangyanan.0428@163.com>
2019-08-07 23:51:11 +0800

    Initial commit

---
+++ src/main/java/com/imadiaos/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.imadiaos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test")
+public class IndexController {
+
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+}
